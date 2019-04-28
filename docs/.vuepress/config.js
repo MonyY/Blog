04b0ef86516e2bbbe06184b31cf2237b869fdf98
郁世昌 <yushichang0845@dingtalk.com>
2019-04-28 19:44:46 +0800

    husky文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,42 +1,37 @@
 module.exports = {
-  title: "Mony",
-  description: "说点什么好呢...",
-  head: [
-    ["link", { rel: "icon", href: `/logo.jpg` }]
-  ],
-  base: "/Mony/",
-  dest: "./dist",
-  evergreen: true,
-  ga: 'UA-125337358-1',
-  themeConfig: {
-    nav: [
-      { text: "首页", link: "/" },
-      { text: "积累", link: "/HTTP/HTTP协议" }
-    ],
-    sidebar: [{
-      title: 'HTTP',
-      children: [
-        '/HTTP/HTTP协议'
-      ]
-    }, {
-      title: 'JS',
-      children: [
-        '/JS/JS-basic-knowledge'
-      ]
-    }, {
-      title: 'Vue',
-      children: [
-        '/Vue/'
-      ]
-    }, {
-      title: 'CSS',
-      children: [
-        '/CSS/'
-      ]
-    }],
-    markdown: {
-      lineNumbers: true
-    },
-    lastUpdated: 'Last Updated'
-  }
+	title: 'Mony',
+	description: '说点什么好呢...',
+	configureWebpack: {
+		resolve: {
+			alias: {
+				public: '/public'
+			}
+		}
+	},
+	head: [['link', { rel: 'icon', href: `/logo.jpg` }]],
+	base: '/Mony/',
+	dest: './dist',
+	evergreen: true,
+	ga: 'UA-125337358-1',
+	themeConfig: {
+		nav: [{ text: '首页', link: '/' }, { text: '积累', link: '/HTTP/HTTP协议' }],
+		sidebar: [
+			{
+				title: 'HTTP',
+				children: ['/HTTP/HTTP']
+			},
+			{
+				title: 'JS',
+				children: ['/JS/JS-basic-knowledge']
+			},
+			{
+				title: 'Vue',
+				children: ['/Vue/vue-cli', '/Vue/husky']
+			}
+		],
+		markdown: {
+			lineNumbers: true
+		},
+		lastUpdated: 'Last Updated'
+	}
 };
